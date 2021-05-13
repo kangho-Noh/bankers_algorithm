0978b429e7a73a88d89ea1237269c7a50a2c9d80
kangho-Noh <rkdgh98@khu.ac.kr>
2021-05-13 21:21:07 +0900

    now the program can all safe sequences.

--- main.cpp
+++ main.cpp
@@ -12,27 +12,28 @@ vector<vector<int>> Need(NUM_OF_PROCESS);
 vector<int> available;
 vector<int> temp_available;
 vector<int> safe_sequence;
+vector<vector<int>> safe_sequences;
 bool found;
 int chk[NUM_OF_PROCESS];
 
 /*
 * 1번문제 input
 
-6 0 1 2 4 0 0 1
-1 7 5 0 1 1 0 0
-2 3 5 6 1 2 5 4
-1 6 5 3 0 6 3 3
-1 6 5 6 0 2 1 2
+6 0 1 2  4 0 0 1
+1 7 5 0  1 1 0 0
+2 3 5 6  1 2 5 4
+1 6 5 3  0 6 3 3
+1 6 5 6  0 2 1 2
 3 2 1 1
 
 * 2번문제 input
 
-6 0 1 2 4 0 0 1
-1 7 5 0 1 1 0 0
-2 3 5 6 1 2 5 4
-1 6 5 3 0 6 3 3
-1 6 5 6 1 4 1 2
-3 2 1 1
+6 0 1 2  4 0 0 1
+1 7 5 0  1 1 0 0
+2 3 5 6  1 2 5 4
+1 6 5 3  0 6 3 3
+1 6 5 6  1 4 1 2
+2 0 1 1
 
 */
 bool can_alloc(int pid) {
@@ -47,29 +48,30 @@ void avail_plus_alloc(int pid) {
 		available[i] += Alloc[pid][i];
 	}
 }
+void avail_minus_alloc(int pid) {
+	for (int i = 0; i < SHARED_RESOURCES; i++) {
+		available[i] -= Alloc[pid][i];
+	}
+}
 
 void dfs(int now) {
 	chk[now]++;
 	avail_plus_alloc(now);
 	safe_sequence.push_back(now);
 	if (safe_sequence.size() == NUM_OF_PROCESS) {
-		for (int i = 0; i < 4; i++) {
-			cout << available[i] << ' ';
-		}
-		cout << "available\n";
-		found = true;
-		return;
+		safe_sequences.push_back(safe_sequence);
 	}
 	for (int i = 0; i < NUM_OF_PROCESS;i++) {
-		if (!chk[i] && can_alloc(i)) {
+		if (!chk[i] && can_alloc(i) ) {
 			dfs(i);
 		}
-		if (found) return;
 	}
+	chk[now]--;
+	avail_minus_alloc(now);
 	safe_sequence.pop_back();
 }
 
-void dfs_all() {
+void bankers_algorithm() {
 	for (int i = 0; i < NUM_OF_PROCESS; i++) {
 		safe_sequence.clear();
 		available = temp_available;
@@ -77,7 +79,6 @@ void dfs_all() {
 		if (can_alloc(i)) {
 			dfs(i);
 		}
-		if (found) return;
 	}
 	return;
 }
@@ -86,6 +87,7 @@ int main() {
 	cin.tie(NULL); ios_base::sync_with_stdio(false);
 	found = false;
 	int a;
+	cout << "Max & Allocation input:\n";
 	for (int i = 0; i < NUM_OF_PROCESS; i++) {
 		for (int j = 0; j < SHARED_RESOURCES; j++) {
 			cin >> a;
@@ -99,27 +101,26 @@ int main() {
 			Need[i].push_back(Max[i][j] - Alloc[i][j]);
 		}
 	}
+	cout << "Available:\n";
 	for (int i = 0; i < SHARED_RESOURCES; i++) {
 		cin >> a;
 		temp_available.push_back(a);
 	}
-	//for (int i = 0; i < NUM_OF_PROCESS; i++) {
-	//	for (int j = 0; j < SHARED_RESOURCES; j++) {
-	//		cout << Max[i][j] << ' ';
-	//	}
-	//	cout << '\n';
-	//}
-	dfs_all();
 
-	if (found) {
-		cout << "Safe Sequence가 존재합니다.\n";
-		for (int i = 0; i < NUM_OF_PROCESS; i++) {
-			cout << "PID"<<safe_sequence[i] << ' ';
-			if (i != NUM_OF_PROCESS - 1)cout << "-> ";
+	bankers_algorithm();
+
+	if (!safe_sequences.empty()) {
+		cout << "\nResult : Safe\n";
+		cout << "\n == Safe sequences == \n";
+		for (vector<int> v : safe_sequences) {
+			for (int i = 0; i < v.size();i++) {
+				cout << "PID" << v[i] << ' ';
+				if (i != v.size() - 1) cout << "-> ";
+			}
+			cout << '\n';
 		}
-		cout << '\n';
 	}
 	else {
-		cout << "Safe Sequence가 존재하지 않습니다.\n";
+		cout << "\nResult : Unsafe\n";
 	}
 }
